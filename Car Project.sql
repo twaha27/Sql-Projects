c67194731a85863eb673c6c715f80528f59b8bd2
twaha27 <144166370+twaha27@users.noreply.github.com>
2023-09-06 10:43:01 +0530

    Update Car Project.sql

--- Car Project.sql
+++ Car Project.sql
@@ -3,9 +3,11 @@ use cars;
 select * from car_dekho;
 -- Total Cars: To get a count total record--
 select count(*) from car_dekho; 
+output: 7927
 -- The manager ask the employee how many will be availbale in 2023--
 select count(*) from car_dekho 
 where year = 2023;
+Output: 6 
 -- The manager ask the employee how many will be availbale in 2020, 2021, 2022--
 select count(*) from car_dekho 
 where year in (2020, 2021, 2022)
@@ -20,6 +22,36 @@ where Year = 2020 and fuel = "Diesel";
 select year, count(*) from car_dekho 
 where fuel = "Petrol" or fuel = "Diesel" or Fuel = "CNG"
 group by year;
+output: 2023	5
+        2022	7
+        2021	7
+        2020	74
+        2019	583
+        2018	806
+        2017	1010
+        2016	856
+        2015	773
+        2014	620
+        2013	666
+        2012	611
+        2011	566
+        2010	366
+        2009	231
+        2008	198
+        2007	169
+        2006	101
+        2005	76
+        2004	51
+        2003	37
+        2002	19
+        2001	6
+        2000	16
+        1999	14
+        1998	9
+        1997	9
+        1996	2
+        1995	1
+        1994	2
 -- Manager said there were more than cars in a given year, which year had more than 100 cars ?--  
 select year, count(*) from car_dekho 
 group by year 
