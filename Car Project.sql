1c2f52e9ce7f6bde6040ce7ce32d6872eb6666db
Ahmed Twaha <144166370+twaha27@users.noreply.github.com>
2023-09-09 12:27:53 +0530

    Update Car Project.sql

--- Car Project.sql
+++ Car Project.sql
@@ -1,23 +1,29 @@
 use cars;
 -- Read Car data--
 select * from car_dekho;
+
 -- Total Cars: To get a count total record--
 select count(*) from car_dekho; 
 output: 7927
+
 -- The manager ask the employee how many will be availbale in 2023--
 select count(*) from car_dekho 
 where year = 2023;
 Output: 6 
+
 -- The manager ask the employee how many will be availbale in 2020, 2021, 2022--
 select count(*) from car_dekho 
 where year in (2020, 2021, 2022)
 group by year;
+
 -- Clint asked me to print the total of all cars by year,I dont see all the details--
 select year, count(*) from car_dekho 
 group by year
+
 -- Clint ask to car dealers how many diesel cars will there in 2020?--
 select count(*) from car_dekho 
 where Year = 2020 and fuel = "Diesel";
+
 -- The manager told the employee to give a print all the cars (petrol, diesel and CNG) come by all year--
 select year, count(*) from car_dekho 
 where fuel = "Petrol" or fuel = "Diesel" or Fuel = "CNG"
@@ -52,12 +58,15 @@ output: 2023	5
         1996	2
         1995	1
         1994	2
+
 -- Manager said there were more than cars in a given year, which year had more than 100 cars ?--  
 select year, count(*) from car_dekho 
 group by year 
 having count(*)>100;
+
 -- The manager said to the employee all the cars details B/W 2015 & 2023; we need complete list --
 select count(*) from car_dekho where year between 2015 and 2023;
+
 -- The manager said to the employee all the cars details b/w 2015 to 2023 we need cpmlete list;
 select * from car_dekho 
 where year between 2015 and 2023;
